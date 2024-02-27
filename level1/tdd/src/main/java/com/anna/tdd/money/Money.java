f6780649296d4d96ab1b4f4db03850acdfce2d4b
MingyeomKim <mingyum119@naver.com>
2024-02-27 14:57:41 +0900

    refactor: Money에서 Dollar와 Franc의 생성자를 호출하도록 수정

--- level1/tdd/src/main/java/com/anna/tdd/money/Money.java
+++ level1/tdd/src/main/java/com/anna/tdd/money/Money.java
@@ -3,6 +3,14 @@ package com.anna.tdd.money;
 class Money {
     protected int amount;
 
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Franc franc(int amount) {
+        return new Franc(amount);
+    }
+
     public boolean equals(Object object) {
         Money money = (Money) object;
         return money.amount == amount && money.getClass() == this.getClass();
--- level1/tdd/src/test/java/com/anna/tdd/money/DollarTest.java
+++ level1/tdd/src/test/java/com/anna/tdd/money/DollarTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 class DollarTest {
     @Test
     void testMultiplication() {
-        Dollar five = new Dollar(5);
+        Money five = new Dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
