8be91633d310bb7264b1fd488357a226e55269f6
MingyeomKim <mingyum119@naver.com>
2024-02-28 14:34:15 +0900

    refactor: Dollar와 Franc의 직접 참조를 제거

--- level1/tdd/src/main/java/com/anna/tdd/money/Dollar.java
+++
@@ -1,7 +0,0 @@
-package com.anna.tdd.money;
-
-public class Dollar extends Money{
-    public Dollar(final int amount, final String currency) {
-        super(amount, currency);
-    }
-}
--- level1/tdd/src/main/java/com/anna/tdd/money/Franc.java
+++
@@ -1,7 +0,0 @@
-package com.anna.tdd.money;
-
-public class Franc extends Money {
-    public Franc(final int amount, final String currency) {
-        super(amount, currency);
-    }
-}
